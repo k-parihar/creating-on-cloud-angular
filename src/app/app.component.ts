a98b507217b88690320bcdfad7e1f5b981ee4647
k-parihar <96866951+k-parihar@users.noreply.github.com>
2022-09-10 12:36:20 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component, VERSION } from '@angular/core';
-// import rollADie from 'roll-a-die';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
-  // const rollADie = require('roll-a-die');
+  ngOnInit() {
+    const diceRoller = new DiceRoller();
+
+    //	Returns the total rolled value
+    const roll = diceRoller.rollValue('2d20kh1');
+    console.log(roll);
+
+    //	Returns an object representing the dice roll, use to display the component parts of the roll
+    const rollObject = diceRoller.roll('2d20kh1');
+    console.log(rollObject.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-// import rollADie from 'roll-a-die';
 import { DiceRoller } from 'dice-roller-parser';
 
 import { AppComponent } from './app.component';
